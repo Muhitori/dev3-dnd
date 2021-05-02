e2c3eb394d4091b0b43ea7f87d1888d1f3810dc6
Muhitori <turevichkolya@gmail.com>
2021-05-02 16:22:44 +0300

    fixed moving & collision

--- index.js
+++ index.js
@@ -1,75 +1,98 @@
 (() => {
     const mainDiv = document.querySelector('.main_div');
     const input = document.getElementById('main_input');
+    let coordsArr = [];
 
     input.addEventListener("keypress", (e) => {
         if (e.key == "Enter") {
             input.value
                 .split("")
                 .forEach(letter => {
-                    const div = document.createElement("div");
+                    let div = document.createElement("div");
                     div.innerText = letter;
                     div.classList.add("letter");
+                    div.draggable = false;
 
                     div.onclick = (e) => {
-                        document.onclick = (e) => {
-                            makeAllWhite();
-                            div.style.backgroundColor = 'yellow';
+                        makeDraggableRed();
+                        div.style.backgroundColor = 'yellow';
 
-                            if (div.style.position != 'absolute') {
-                                div.style.position = 'absolute';
-                                return;
-                            }
-
-                            moveAt(e, div);
-                            div.onclick = null;
+                        if (div.style.position != 'absolute') {
+                            div.style.position = 'absolute';
                         }
-                    };
 
+                        coordsArr.push({
+                            x: div.style.left,
+                            y: div.style.top
+                        });
+
+                        toggleDrag(div);
+                    };
                     mainDiv.appendChild(div);
                 });
         }
     });
 
-    function moveAt(e, elem) {        
-        const collisionElem = getCollisionElem(e.pageX, e.pageY, elem);
+    document.onclick = (e) => {
+        const div = document.elementFromPoint(e.pageX, e.pageY);
 
-        if (collisionElem && collisionElem.style.position == 'absolute') {
-            collisionElem.style.left = elem.style.left;
-            collisionElem.style.top = elem.style.top;
-            elem.style.backgroundColor = 'white';
-            collisionElem.style.backgroundColor = 'yellow';
-
-            document.onclick = (e) => {
-                moveAt(e, collisionElem);
-                collisionElem.onclick = null;
-            }
+        if (div === input || div === mainDiv){
+            return;
         }
 
+        const collisionElem = getCollisionElem(div);
+
+        if (collisionElem && collisionElem.style.position == 'absolute') {
+            collisionElem.style.left = coordsArr[coordsArr.length - 2].x;
+            collisionElem.style.top = coordsArr[coordsArr.length - 2].y;
+        }
+
+    }
+
+    function toggleDrag(div) {
+        if (!document.onmousemove) {
+            document.onmousemove = (e) => {
+                moveAt(e, div);
+            }
+        } else {
+            document.onmousemove = null;
+            makeDraggableRed();
+        }
+    }
+
+    function moveAt(e, elem) {      
         elem.style.left = e.pageX - elem.offsetWidth / 2 + 'px';
         elem.style.top = e.pageY - elem.offsetHeight / 2 + 'px';
     }
 
-    function getCollisionElem(x, y, elem) {
+    function getCollisionElem(elem) {
+        const rect = elem.getBoundingClientRect();
+
+        const x = rect.top + window.scrollX;
+        const y = rect.left + window.scrollY;
+
         return [...document.querySelectorAll(".letter")]
             .filter(letter => letter != elem)
-            .find(letter => collision(x, y, letter));
+            .find(letter => isCollide(x, y, letter));
     }
 
-    function collision(x, y, letter) {
-        const sizeY = letter.offsetHeight;
-        const sizeX = letter.offsetWidth;
+    function isCollide(x, y, letter) {
         const rect = letter.getBoundingClientRect();
+
+        const sizeY = rect.height;
+        const sizeX = rect.width;
+        const letterX = rect.top + window.scrollX;
+        const letterY = rect.left + window.scrollY;
         
-        return (rect.top < y && y < rect.top + sizeY) && 
-                (rect.left < x && x < rect.left + sizeX);
+        return x < letterX + sizeX && x + sizeX > letterX &&
+                y < letterY + sizeY && y + sizeY > letterY;
     }
 
-    function makeAllWhite() {
+    function makeDraggableRed() {
         [...document.querySelectorAll(".letter")]
         .forEach(letter => {
-            if (letter.style.backgroundColor != 'white')
-                letter.style.backgroundColor = 'white';
-        })
+            if (letter.style.backgroundColor != 'red' && letter.style.position == 'absolute')
+                letter.style.backgroundColor = 'red';
+        });
     }
 })();
